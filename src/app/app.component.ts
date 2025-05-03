7822552aa5a62edb7832d773df9ab6df8a1fd710
tritri <sakaitri@gmail.com>
2025-05-03 14:36:25 +0900

    Angularでとりあえずコンポーネントを追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component } from '@angular/core';// ルーティングによる画面遷移のために必要なモジュール
+import { RouterModule, Routes } from '@angular/router';
+
 
 @Component({
+  // このコンポーネントを表すタグ
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  // このコンポーネントに対応するHTML（テンプレート）
+  templateUrl: './app.component.html',
+  // このコンポーネントが使用するCSS
+  styleUrls: [ './app.component.css' ]
 })
+
 export class AppComponent {
-  value = 'Worldddd';
-  
-  constructor(private http: HttpClient) {
-    this.http.get('/api/message')
-      .subscribe((resp: any) => this.value = resp.text);
-  }
 }
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,29 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+// ルーティングによる画面遷移のために必要なモジュール
+import { RouterModule, Routes } from '@angular/router';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from "./app.component";
+import { ApplicationComponent } from './application/application.component';
 
+// Routing を行う対象のコンポーネントを管理する
+// path にセットした文字列にマッチしたURLが指定されると、対になっているコンポーネントが表示される
+// 下記のように明示する以外にも
+//    '' で [/] のルートパスを指定できる
+//    '**' でワイルドカードを指定できる
+const ROUTE_TABLE: Routes = [
+  { path: 'page-a', component: ApplicationComponent }
+]
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent, 
+    ApplicationComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(ROUTE_TABLE)
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
