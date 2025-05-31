92631585abe346a963c26d63fc24f48f85624f75
tritri <sakaitri@gmail.com>
2025-05-31 17:16:15 +0900

    apiプロジェクトを追加

---
+++ api/HttpTriggerEnvironment.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Company.Function;
+
+public class HttpTriggerEnvironment
+{
+    private readonly ILogger<HttpTriggerEnvironment> _logger;
+
+    public HttpTriggerEnvironment(ILogger<HttpTriggerEnvironment> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("HttpTriggerEnvironment")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult("Welcome to Azure Functions!");
+    }
+}
