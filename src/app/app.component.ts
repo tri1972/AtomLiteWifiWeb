3a33fa80d76739634a355404446d73d3a2bbf4b8
tritri <sakaitri@gmail.com>
2025-06-17 22:02:45 +0900

    SideNabをAngularMaterialに切り替え

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,21 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';// ルーティングによる画面遷移のために必要なモジュール
 //import { RouterModule, Routes } from '@angular/router';
 
-import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+//import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { ApplicationComponent } from './application/application.component';
 //import { AppRoutingModule } from './app-routing.module';  //デフォルトでimportされている
 import { RouterModule } from '@angular/router';
+
+import { NavigationComponent } from './navigation/navigation.component';
 
 
 @Component({
     // このコンポーネントを表すタグ
     selector: 'app-root',
     imports: [
-      NgbNavModule,
+      //NgbNavModule,
       RouterModule,//html内で <router-outlet>を使うために必要
+      NavigationComponent
     ],
     // このコンポーネントに対応するHTML（テンプレート）
     templateUrl: './app.component.html',
