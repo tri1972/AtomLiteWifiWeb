082ee9c3043f452995a734288043c29a976c7caa
tritri <sakaitri@gmail.com>
2025-07-17 22:01:55 +0900

    デバッグ環境を整える

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,14 +10,35 @@ import { Component } from '@angular/core';
 
 
 export class HomeComponent {
+  
+  private http:HttpClient;
+  URL='http://localhost:7071/api/HttpTriggerEnvironment'
   message='日本晴れ';
 
   constructor(http:HttpClient) {
-    http.get('/api/HttpTriggerEnvironment')
+    this.http=http;
+    /*
+    http.get(this.URL)
       .subscribe((resp: any) => this.message = resp.text);
-    /*  
-    http.get('/api/message')
-      .subscribe((resp: any) => this.message = resp.text);
-      */
+    */
+  }
+  
+
+  getMethod(){
+    //http.getをするためのオブジェクトを生成
+    var httpObj = this.http.get(this.URL);
+    this.message='ボタンが押された';
+    //成功時・失敗時の動作を指定する。
+    httpObj.subscribe((resp: any) => this.message = resp.text,this.getError);
+  }
+
+  //http.getが成功した時に走るメソッド
+  getSuccess(response:Response){
+    console.log(response.text());
+  }
+
+  //http.getが失敗した時に走るメソッド
+  getError(error){
+    console.log(error);
   }
 }
