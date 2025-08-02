e9df952d99999451aab7844a76edb21e048edaec
tritri <sakaitri@gmail.com>
2025-08-02 16:24:06 +0900

    画面に受け取ったデータをすべて出すよう修正

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,10 +12,13 @@ import { Component } from '@angular/core';
 export class HomeComponent {
   
   private http:HttpClient;
-  //URL='https://httpbin.org/ip'
   URL='/api/HttpTriggerEnvironment';
   //URL='http://localhost:7071/api/HttpTriggerEnvironment'
-  message='日本晴れ';
+  time=''
+  temperature='';
+  humidity='';
+  pressure='';
+  rainDrop='';
 
   constructor(http:HttpClient) {
     this.http=http;
@@ -29,12 +32,16 @@ export class HomeComponent {
   getMethod(){
     //http.getをするためのオブジェクトを生成
     var httpObj = this.http.get(this.URL);
-    this.message='ボタンが押された';
+    this.temperature='ボタンが押された';
     //成功時・失敗時の動作を指定する。
     httpObj.subscribe
     (
       (resp: any) => {
-        this.message = resp.Body.Temperature;
+        this.time = resp.EnqueuedTimeUtc;
+        this.temperature = resp.Body.Temperature;
+        this.humidity = resp.Body.humidity;
+        this.pressure = resp.Body.Pressure;
+        this.rainDrop = resp.Body.Rain;
       },
       (error) => {//http.getが失敗した時に走るメソッド
         console.error('Error occurred:', error);
